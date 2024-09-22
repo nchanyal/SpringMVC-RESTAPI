f1f5f36302ccb41de172c819a5cd1e798bdb7679
Nathnael Chanyalew <134805961+nchanyal@users.noreply.github.com>
2024-09-22 12:43:56 -0400

    Add validations when creating runs

--- server/src/main/java/com/runnerz/run_tracker/Run.java
+++ server/src/main/java/com/runnerz/run_tracker/Run.java
@@ -9,4 +9,19 @@ public record Run(
     LocalDateTime completedOn,
     Integer miles,
     Location location
-) {}
+) {
+    public Run {
+        if(id.intValue() <= 0){
+            throw new IllegalArgumentException("Id must be postive");
+        }
+        if(title.isBlank()){
+            throw new IllegalArgumentException("Title must not be empty");
+        }
+        if(startedOn.isAfter(completedOn)){
+            throw new IllegalArgumentException("Completed On must be after Started On");
+        }
+        if(miles.intValue() <= 0){
+            throw new IllegalArgumentException("Miles must be postive");
+        }
+    }
+}
